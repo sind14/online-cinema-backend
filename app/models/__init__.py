c8ba513149a08047ef9127c70729436ecc44162c
Anton <grygorenko23@gmail.com>
2026-02-25 15:56:37 +0100

    implement user, roles and token models

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from .user import User
+from .user_profiles import UserProfile
+from .user_group import UserGroup
+from .activation_tokens import ActivationToken
+from .password_reset_tokens import PasswordResetToken
+from .refresh_tokens import RefreshToken
---
+++ app/models/user_group.py
@@ -0,0 +1,18 @@
+import enum
+from sqlalchemy import Column, Integer, Enum
+from sqlalchemy.orm import relationship
+from app.database.base import Base
+
+
+class UserGroupEnum(str, enum.Enum):
+    USER = "USER"
+    MODERATOR = "MODERATOR"
+    ADMIN = "ADMIN"
+
+
+class UserGroup(Base):
+    __tablename__ = "user_groups"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Enum(UserGroupEnum, name="user_group_enum"), unique=True, nullable=False)
+    users = relationship("User", back_populates="group")
