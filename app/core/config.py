8cce94ff4f510302e676edcd6d10be4b3fb5974a
Anton <grygorenko23@gmail.com>
2026-03-06 01:42:29 +0100

    switch from GmaiL OAuth to SMTP

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,20 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     REDIS_URL: str
     DATABASE_URL: str
+    SMTP_HOST: str
+    SMTP_PORT: int
+    SMTP_USER: str
+    SMTP_PASS: str
+
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings() # type: ignore
